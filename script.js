872f0713d2a6a3f30befc80eb68bd69930328e32
Inashi100 <Inashi100@users.noreply.github.com>
2023-08-10 10:59:32 +0100

    link javascript to HTML

---
+++ script.js
@@ -0,0 +1,15 @@
+let operator = ''
+
+document.addEventListener("DOMContentLoaded", function(){
+    //store all components on html in our Js
+    let clear = document.querySelector("#clear-btn");
+    let equal = document.querySelector(".equal");
+    let decimal = document.querySelector(".decimal");
+
+    let numbers = document.querySelectorAll("number")
+    let operators = document.querySelectorAll("operators");
+    
+    let previousScreen = document.querySelector(".previousScreen");
+    let currentScreen = document.querySelector(".current");
+
+})
